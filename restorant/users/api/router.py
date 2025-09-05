8ddcfcc308c555cf2e813a6a73693c4312f79103
Marcelo Bravo <mabc@live.cl>
2025-09-05 14:33:15 -0400

    Se creó el endpoint para usuarios

--- restorant/users/api/router.py
+++ restorant/users/api/router.py
@@ -1,5 +1,11 @@
+
+from django.url import path
 from rest_framework.routers import DefaultRouter
-from users.api.views import UsersApiViewSet
+from users.api.views import UsersApiViewSet, UserView
 
 router_user = DefaultRouter()
 router_user.register(prefix='users', basename='users', viewset= UsersApiViewSet)
+
+urlpatterns = [
+    path('auth/me/', UserView.as_view())
+]
